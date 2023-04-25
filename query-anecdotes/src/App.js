8a6706603448dd70c19bbe17bba1c26236be01bd
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-04-25 12:22:48 +0900

    Complete 6.20

---
+++ query-anecdotes/src/App.js
@@ -0,0 +1,46 @@
+import { useQuery } from 'react-query'
+import { getAnecdotes } from './requests'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+
+  const handleVote = (anecdote) => {
+    console.log('vote')
+  }
+
+  const result = useQuery('anecdotes', getAnecdotes)
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  if ( result.isError ) {
+    return <span>Server Error: Anecdote service not available</span>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+
+      <Notification />
+      <AnecdoteForm />
+
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
